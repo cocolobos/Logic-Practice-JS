ae69878df5d86341507e1e19bfa05c61aec931b8
cocolobos <nicolasmlobos@gmail.com>
2020-10-25 12:39:44 -0300

    code fixed in exercise a

--- funciones.js
+++ funciones.js
@@ -1,9 +1,8 @@
 //  6-a)Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
-var x=suma;
 function suma(a,b){
     return a+b;
 }
-console.log(x);
+console.log(suma(25,9));
 //  6-b)A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
 var x=suma;
 function suma(a,b){
