e711b6bdd0e56064ee4f4ac28544d2ed31c8ecba
cocolobos <nicolasmlobos@gmail.com>
2020-10-25 11:24:07 -0300

    Variables block of exercise

---
+++ variables-operadores.js
@@ -0,0 +1,19 @@
+//  1-a)-Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+var num1,num2;
+var num3=num1+num2;
+//  1-b)Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+var firstName="Nicolas ";
+var lastName="Lobos";
+var fullName=firstName + lastName;
+//  1-c)Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
+var carName="Peugeot";
+var carColor="Blue";
+var totalLength=carName.length+carColor.length;
+
+
+
+
+
+ 
+
+
